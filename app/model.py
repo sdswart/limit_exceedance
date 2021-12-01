0efbd485f5fd3c557e44674662d7d169798b92b1
Stephen <sswart@gastops.com>
2021-12-01 01:59:36 -0500

    update

--- app/model.py
+++ app/model.py
@@ -33,47 +33,88 @@ def get_samples_limits(config):
         limits=json.load(f)
     return samples,limits
 
-def predict(config,samples,limits):
-    #Get Counts limits
-    levels=[(-1,'Healthy')]
+def get_urgency_priorities(limits):
+    urgencies=[]
     for limit in limits:
-        if 'Property' in limit and limit['Property']=='Counts' and 'LimitLevel' in limit:
-            for limitlevel in limit['LimitLevel']:
-                levels.append((limitlevel['Upper'],limitlevel['Urgency']))
-    levels=sorted(levels,key=lambda x: x[0])
-    def get_value_urgency(val):
-        for level in levels:
-            if val>=level[0]:
-                return level[1]
+        level_key=([key for key in limit.keys() if 'level' in key.lower()]+[None])[0]
+        if level_key is not None:
+            limit_levels=limit[level_key]
+            for x in limit_levels:
+                x['upperlower']=x.get('Upper') or x.get('upper') or -x.get('Lower') or -x.get('lower')
+            limit_levels=sorted([x for x in limit_levels if x['upperlower'] is not None],key=lambda x: x['upperlower'])
+
+            for limitlevel in limit_levels:
+                cur_urgency=limitlevel.get('Urgency') or limitlevel.get('urgency')
+                if cur_urgency is not None and cur_urgency.lower() not in urgencies:
+                    urgencies.append(cur_urgency.lower())
+    return urgencies
+
+def get_equipment_limits(equipment_id,limits,property_name):
+    levels=dict(upper=[],lower=[],urgency=[])
+    for limit in limits:
+        limit_equipment=limit.get('Equipment') or limit.get('equipment')
+        if type(limit_equipment) not in [list,tuple,np.ndarray]:
+            limit_equipment=[limit_equipment]
+        if limit_equipment is None or equipment_id in limit_equipment:
+            prop = limit.get('Property') or limit.get('property')
+            level_key=([key for key in limit.keys() if 'level' in key.lower()]+[None])[0]
+            if prop is not None and level_key is not None and prop.lower()==property_name.lower():
+                for limitlevel in limit[level_key]:
+                    levels['upper'].append(limitlevel.get('Upper') or limitlevel.get('upper') or np.inf)
+                    levels['lower'].append(limitlevel.get('Lower') or limitlevel.get('lower') or -np.inf)
+                    levels['urgency'].append(limitlevel.get('Urgency') or limitlevel.get('urgency'))
+    return {key:np.array(vals) for key,vals in levels.items()}
+
+def predict(config,samples,limits):
     #Loop through samples
     cur_t=0;cur_urgency=None;cur_sample=None
     conditions=[]
+    ordered_urgencies=get_urgency_priorities(limits)
+    conditions=[]
     for sample in samples:
-        if 'Data' in sample and 'Counts' in sample['Data']:
-            counts=sample['Data']['Counts']
-            counts=sorted(counts,key=lambda x: x['t'])
-            t=[];y=[]
-            for ty in counts:
-                t.append(ty['t'])
-                y.append(ty['y'])
-            t=np.array(t); y=np.array(y)
-            for level in levels:
-                pos_ts=np.where(y>=level[0])[0]
-                if len(pos_ts)>0:
-                    pos=pos_ts[0]
-                    if sample['_id']!=cur_sample or (t[pos]>cur_t and level[1]!=cur_urgency):
-                        cur_t=t[pos]
-                        cur_urgency=level[1]
-                        cur_sample=sample['_id']
-                        conditions.append({
-                                            'Sample':cur_sample,
-                                            'Condition':cur_urgency,
-                                            'Timestamp':cur_t,
-                                            'Version':config.VERSION,
-                                        })
-                        if config.EXTRA_RETURN_ARGS:
-                            conditions[-1]['Extra']=config.EXTRA_RETURN_ARGS
-    return conditions
+        sample_equipment=sample.get('Equipment') or sample.get('equipment')
+        if type(sample_equipment) not in [list,tuple,np.ndarray]:
+            sample_equipment=[sample_equipment]
+        data = sample.get('Data') or sample.get('data')
+
+        if data:
+            for property_name, values in data.items():
+                t,y=zip(*[(x['t'],x['y']) for x in sorted(values,key=lambda x: x['t'])])
+                y=np.array(y)
+                t=np.array(t)
+
+                for equipment_id in sample_equipment:
+                    equipment_limits = get_equipment_limits(equipment_id,limits,property_name)
+                    y_upper=y-equipment_limits['upper'].reshape((-1,1))
+                    y_lower=equipment_limits['lower'].reshape((-1,1))-y
+                    y_all=np.vstack([y_upper,y_lower])
+                    y_all=np.where(np.logical_or(np.isnan(y_all),y_all<0),np.inf,y_all)
+                    pos=~np.all(np.isinf(y_all),axis=0)
+                    ind=np.argmin(y_all,axis=0)
+                    urgency=np.tile(equipment_limits['urgency'],2)
+                    cur_conditions=np.where(pos,urgency[ind],None)
+
+                    prev_cond=None
+                    for t,condition in zip(t[pos],urgency[ind][pos]):
+                        if prev_cond is None or condition!=prev_cond:
+                            conditions.append({
+                                'Equipment_id':equipment_id,
+                                'Property':property_name,
+                                'Sample':sample['_id'],
+                                'Condition':condition,
+                                'Timestamp':t,
+                                'Version':config.VERSION,
+                                'Extra':(config.EXTRA_RETURN_ARGS if config.EXTRA_RETURN_ARGS else {}),
+                            })
+                            prev_cond=condition
+    #Clean up
+    conditions=sorted(conditions,key=lambda x: x['Timestamp'])
+    res=[]
+    for condition in conditions:
+        prev_cond=[x for x in res if x['Timestamp']<=condition['Timestamp'] and x['Equipment_id']<=condition['Equipment_id']]
+        if len(prev_cond)==0 or (condition['Property']==prev_cond[-1]['Property'] and condition['Condition']!=prev_cond[-1]['Condition']) or (condition['Property']!=prev_cond[-1]['Property'] and ordered_urgencies.index(condition['Condition'].lower())>ordered_urgencies.index(prev_cond[-1]['Condition'].lower())):
+            res.append(condition)
+    return res
 
 def run_algorithm(config):
     samples,limits=get_samples_limits(config)
